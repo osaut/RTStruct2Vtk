f113ba9e5e69c936cb3e8102ec860e3b69c0e814
Olivier Saut <osaut@airpost.net>
2013-11-05 13:48:11 +0100

    Add a ruby script for batch processing

---
+++ process.rb
@@ -0,0 +1,20 @@
+exename = File.absolute_path("./rtsr.out")
+fail unless File.exists?(exename)
+
+Dir.chdir(ARGV[0])
+patients=Dir.glob("patient*")
+patients.each { |patient|
+    puts "Processing #{patient}..."
+        Dir.mkdir("./volumes") unless Dir.exists?("./volumes")
+        exams=Dir.glob("#{patient}/p_*")
+        exams.each { |exam|
+            contours=Dir.glob("#{exam}/RTSTRUCT.*.dcm")
+            contours.each { |cc|
+                dirname=File.join(ARGV[0], patient,"volumes", File.basename(cc,".dcm").sub(/RTSTRUCT./, ''))
+                Dir.mkdir(dirname) unless Dir.exists?(dirname)
+
+                dcm_file=File.absolute_path(cc)
+                system  "#{exename} #{dcm_file} #{dirname}"
+            }
+        }
+}
