81737d20b7c4d0d710d17db249aec62eafcaa391
Olivier Saut <osaut@airpost.net>
2013-11-25 19:21:50 +0100

    On prend en compte les dates (même si les données actuelles sont erronées).

--- process.rb
+++ process.rb
@@ -1,3 +1,12 @@
+require 'dicom'
+require 'date'
+include DICOM
+
+def get_date fname
+    dcm=DObject.read(fname)
+    Date.parse(dcm.value("0008,0020"))
+end
+
 exename = File.absolute_path("./rtsr.out")
 fail unless File.exists?(exename)
 
@@ -9,11 +18,18 @@ patients.each { |patient|
         exams=Dir.glob("#{patient}/p_*")
         exams.each { |exam|
             contours=Dir.glob("#{exam}/RTSTRUCT.*.dcm")
+            dates=contours.map { |f| get_date(f)}.uniq
+
             contours.each { |cc|
-                dirname=File.join(ARGV[0], patient,"volumes", File.basename(cc,".dcm").sub(/RTSTRUCT./, ''))
-                Dir.mkdir(dirname) unless Dir.exists?(dirname)
 
                 dcm_file=File.absolute_path(cc)
+                if (contours.size==dates.size)
+                    date=get_date(dcm_file)
+                    dirname=File.join(ARGV[0], patient,"volumes", date.strftime('%Y_%m_%d'))
+                else
+                    dirname=File.join(ARGV[0], patient,"volumes", File.basename(cc,".dcm").sub(/RTSTRUCT./, ''))
+                end
+                Dir.mkdir(dirname) unless Dir.exists?(dirname)
                 system  "#{exename} #{dcm_file} #{dirname}"
             }
         }
